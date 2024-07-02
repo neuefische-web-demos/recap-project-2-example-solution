3a57dfa59082d8db0ab47d5fb70b59f53a1a828d
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-07-02 14:39:25 +0200

    add toggle functionality to all cards

--- index.js
+++ index.js
@@ -6,21 +6,26 @@ function getByDataJS(dataJsTag, element) {
   return element.querySelector(`[data-js='${dataJsTag}']`);
 }
 
-const card = getByDataJS("card");
-const answerButton = getByDataJS("answer-button", card); // answerButton is only searched on the "card" element instead inside the whole "document"
-const answerDisplay = getByDataJS("answer", card);
-const bookmark = getByDataJS("bookmark", card);
+function addCardInteractivity(cardElement) {
+  const answerButton = getByDataJS("answer-button", cardElement); // answerButton is only searched on the "cardElement" element instead inside the whole "document"
+  const answerDisplay = getByDataJS("answer", cardElement);
+  const bookmark = getByDataJS("bookmark", cardElement);
 
-answerButton.addEventListener("click", () => {
-  answerDisplay.classList.toggle("card__answer--active");
+  answerButton.addEventListener("click", () => {
+    answerDisplay.classList.toggle("card__answer--active");
 
-  const isAnswerVisible = answerDisplay.classList.contains(
-    "card__answer--active"
-  );
+    const isAnswerVisible = answerDisplay.classList.contains(
+      "card__answer--active"
+    );
 
-  answerButton.textContent = isAnswerVisible ? "Hide Answer" : "Show Answer";
-});
+    answerButton.textContent = isAnswerVisible ? "Hide Answer" : "Show Answer";
+  });
 
-bookmark.addEventListener("click", () => {
-  bookmark.classList.toggle("bookmark--active");
-});
+  bookmark.addEventListener("click", () => {
+    bookmark.classList.toggle("bookmark--active");
+  });
+}
+
+const cards = document.querySelectorAll("[data-js='card']");
+
+cards.forEach((card) => addCardInteractivity(card));
